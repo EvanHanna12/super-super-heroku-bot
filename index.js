eff003c1dc1f13caf03c6628a74796189ae09481
EvanHanna12 <45378721+EvanHanna12@users.noreply.github.com>
2019-04-09 18:31:26 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
+// Log our bot in using the token from https://discordapp.com/developers/applications/me
+client.login(process.env.token);
